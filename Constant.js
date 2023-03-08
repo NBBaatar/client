575dab16024fe7aa91b2966743afefa8b64cab7d
NB <satanhero.nb@gmail.com>
2023-03-08 14:52:05 +1100

    updated

--- Constant.js
+++ Constant.js
@@ -1,3 +1,3 @@
 //export const serverClient = "http://192.168.0.123:8000";
-export const serverClient = "http://192.168.1.8:8000";
-// export const serverClient = "https://level33-server.onrender.com";
+// export const serverClient = "http://192.168.1.8:8000";
+export const serverClient = "https://level33-server.onrender.com";
